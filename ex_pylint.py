b6bd0bcff543615c10ed9433bb4b429f3471a8dc
Otávio Viana Marques dos Santos <otavio_vms@hotmail.com>
2025-11-12 12:50:06 -0300

    config: adding pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,9 @@
+print("Ola Mundo")
+
+def minha_funcao():
+    print("Estou aqui")
+
+
+# snake_case -> variaveis e funcoes em python: my_variable -> Correto
+# camelCase -> nao consta: minhaFuncao -> Errado
+# PascalCase -> classes  em python
